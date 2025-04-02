dd281507df8fcad8df22dbc091c1073e1d8bd3b1
EMANON <2319727090@qq.com>
2025-04-02 11:36:47 +0800

    Spring Boot 自定义响应工具类

--- weblog-web/src/main/java/com/xiaozhou/weblog/web/controller/TestController.java
+++ weblog-web/src/main/java/com/xiaozhou/weblog/web/controller/TestController.java
@@ -1,5 +1,6 @@
 package com.xiaozhou.weblog.web.controller;
 
+import com.xiaozhou.weblog.common.utils.Response;
 import com.xiaozhou.weblog.web.model.User;
 import com.xiaozhou.weblog.common.aspect.ApiOperationLog;
 import lombok.extern.slf4j.Slf4j;
@@ -23,7 +24,7 @@ public class TestController {
 
     @PostMapping("/test")
     @ApiOperationLog(description = "测试接口")
-    public ResponseEntity<String> test(@RequestBody @Validated User user, BindingResult bindingResult) {
+    public Response test(@RequestBody @Validated User user, BindingResult bindingResult) {
         // 是否存在校验错误
         if (bindingResult.hasErrors()) {
             // 获取校验不通过字段的提示信息
@@ -32,11 +33,11 @@ public class TestController {
                     .map(FieldError::getDefaultMessage)
                     .collect(Collectors.joining(", "));
 
-            return ResponseEntity.badRequest().body(errorMsg);
+            return Response.fail(errorMsg);
         }
 
         // 返参
-        return ResponseEntity.ok("参数没有任何问题");
+        return Response.success();
     }
 
 }

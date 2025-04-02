c0f5811356cbdd259d17debc5815984fca42aadf
EMANON <2319727090@qq.com>
2025-04-02 18:41:03 +0800

    自定义了 Jackson 序列化、反序列化，让其支持了 Java 8 新的日期 API , 这些日期新特性相比较 Date, 使用起来会更加方便

--- weblog-web/src/main/java/com/xiaozhou/weblog/web/controller/TestController.java
+++ weblog-web/src/main/java/com/xiaozhou/weblog/web/controller/TestController.java
@@ -2,6 +2,7 @@ package com.xiaozhou.weblog.web.controller;
 
 import com.xiaozhou.weblog.common.enums.ResponseCodeEnum;
 import com.xiaozhou.weblog.common.exception.BizException;
+import com.xiaozhou.weblog.common.utils.JsonUtil;
 import com.xiaozhou.weblog.common.utils.Response;
 import com.xiaozhou.weblog.web.model.User;
 import com.xiaozhou.weblog.common.aspect.ApiOperationLog;
@@ -16,6 +17,9 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
 import java.util.stream.Collectors;
 /**
  * @author 小周
@@ -31,8 +35,15 @@ public class TestController {
     @ApiOperationLog(description = "测试接口")
     @ApiOperation(value = "测试接口")
     public Response test(@RequestBody @Validated User user) {
-        return Response.success();
-    }
+        // 打印入参
+        log.info(JsonUtil.toJsonString(user));
 
+        // 设置三种日期字段值
+        user.setCreateTime(LocalDateTime.now());
+        user.setUpdateDate(LocalDate.now());
+        user.setTime(LocalTime.now());
+
+        return Response.success(user);
+    }
 }
 
